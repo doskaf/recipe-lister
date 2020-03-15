9350a7c5b74f9a5d5a8ccb840b044847947bd68d
Dominique Skaf <doskaf@yahoo.com>
2020-03-14 23:08:03 -0500

    Added fullRender function to Recipe class

--- recipe-lister-frontend/src/recipe.js
+++ recipe-lister-frontend/src/recipe.js
@@ -12,4 +12,14 @@ class Recipe {
 
         Recipe.all.push(this);
     }
+
+    fullRender() {
+        this.element.innerHTML = `
+        <h3>${this.name}</h3>
+        <button class="add-to-list-button">Add to Shopping List</button><br />
+        <ul></ul><br />
+        <button class="edit-recipe-button">Edit</button><br />
+        <button class="delete-recipe-button">Delete Recipe</button>
+        `
+    }
 }
